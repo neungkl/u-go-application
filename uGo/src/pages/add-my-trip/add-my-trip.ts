0f1ceccb49b4a468891518a5a5a3f951d202e291
Ekkalak Leelasornchai <ekkalakfm@gmail.com>
2017-02-04 20:26:35 +0700

    make addtrip2

---
+++ uGo/src/pages/add-my-trip/add-my-trip.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-add-my-trip',
+  templateUrl: 'add-my-trip.html'
+})
+export class AddMyTripPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  
+
+}
